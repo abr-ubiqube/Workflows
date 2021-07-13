aaf3d050a3a0e00d882cb36399eccf3b1d4b0f2a
jboss <jboss@7f305e094948>
2021-07-13 12:39:48 +0000

    Updating Workflow on  - 13-07-2021 12:39:48 - ncroot

--- sdwan_connect_branch/Process_CREATE/Tasks/.meta_Task_task_02_create_configuration.py
+++ sdwan_connect_branch/Process_CREATE/Tasks/.meta_Task_task_02_create_configuration.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1626179903452</value>
+            <value>1626179988412</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1626179903447</value>
+            <value>1626179988407</value>
         </entry>
         <entry>
             <key>TAG</key>
--- sdwan_connect_branch/Process_CREATE/Tasks/Task_task_02_create_configuration.py
+++ sdwan_connect_branch/Process_CREATE/Tasks/Task_task_02_create_configuration.py
@@ -1,54 +1,118 @@
-'''
-Visit http://[YOUR_MSA_URL]/msa_sdk/ to see what you can import.
-'''
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 
-'''
-List all the parameters required by the task
 
-You can use var_name convention for your variables
-They will display automaticaly as "Var Name"
-The allowed types are:
-  'String', 'Boolean', 'Integer', 'Password', 'IpAddress',
-  'IpMask', 'Ipv6Address', 'Composite', 'OBMFRef', 'Device'
-
- Add as many variables as needed
-'''
 dev_var = Variables()
-dev_var.add('var_name', var_type='String')
-dev_var.add('var_name2', var_type='Integer')
+context = Variables.task_call()
 
-'''
-context => Service Context variable per Service Instance
-All the user-inputs of Tasks are automatically stored in context
-Also, any new variables should be stored in context which are used across Service Instance
-The variables stored in context can be used across all the Tasks and Processes of a particular Service
-Update context array [add/update/delete variables] as per requirement
+# (1) loopbacks
+ms_left_loopback_vars = {"object_id": context['id'],
+                     	"ip_address": context['leftsubnet']
+                     	}
 
-ENTER YOUR CODE HERE
-'''
-context = Variables.task_call(dev_var)
-context['var_name2'] = int(context['var_name2']) + 1
+sdwan_loopback_left = {"sdwan_loopback":
+                   	{context['id']: ms_left_loopback_vars}
+                   	}
 
-'''
-Format of the Task response :
-JSON format : {"wo_status":"status","wo_comment":"comment","wo_newparams":{json_body}}
-wo_status : ENDED [Green color] or FAILED [Red color] or WARNING [Orange color]
-			-> While the Task is Running [means no response returned yet], task status is RUNNING [Blue color]
-         -> When status is returned as FAILED, the Orchestration Engine stops the Process Execution from this Task
-wo_comment : Appropriate Comment to display as per the success/failure of the Task
-wo_newparams : json_body parameters returned from this Task
+ms_right_loopback_vars = {"object_id": context['id'],
+                      	"ip_address": context['rightsubnet']
+                      	}
 
-Function process_content() takes care of Creating a Json response from inputs
-This function definiton can be found at : http://[YOUR_MSA_URL]/msa_sdk/msa_api.html#msa_sdk.msa_api.MSA_API.process_content
-NOTE : For 'wo_newparams', always pass "context" [whether wo_status is ENDED/FAILED/WARNING to preserve it across Service Instance]
-    -> Last argument "true" mentions whether the json_response to be Logged in the logfile : /opt/jboss/latest/logs/process.log
-    -> If not passed, it's "false"
+sdwan_loopback_right = {"sdwan_loopback":
+                    	{context['id']: ms_right_loopback_vars}
+                    	}
+# (2) secrets
+ms_left_secret_vars = {"object_id": context['id'],
+                   	"left": context['left_device_ip'],
+                   	"right": context['right_device_ip'],
+                   	"secret": context['secret'],
+                   	}
 
-The response "ret" should be echoed from the Task "print(ret)" which is read by Orchestration Engine
-In case of FAILURE/WARNING, the Task can be Terminated by calling "exit" as per Logic
-'''
-ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
+sdwan_ipsec_secret_left = {"sdwan_ipsec_secret":
+                       	{context['id']: ms_left_secret_vars}
+                       	}
+
+ms_right_secret_vars = {"object_id": context['id'],
+                    	"left": context['right_device_ip'],
+                    	"right": context['left_device_ip'],
+                    	"secret": context['secret'],
+                    	}
+
+sdwan_ipsec_secret_right = {"sdwan_ipsec_secret":
+                        	{context['id']: ms_right_secret_vars}
+                        	}
+
+# (3) global policy
+
+ms_global_policy_vars = {"object_id": context['id'],
+                     	"ikelifetime": "1440m",
+                     	"keylife": "60m",
+                     	"rekeymargin": "3m",
+                     	"keyingtries": "1",
+                     	"keyexchange": "ikev1",
+                     	"authby": "secret"
+                     	}
+
+sdwan_ipsec_conf_left = {"sdwan_ipsec_conf_policy":
+                     	{context['id']: ms_global_policy_vars}
+                     	}
+
+sdwan_ipsec_conf_right = {"sdwan_ipsec_conf_policy":
+                      	{context['id']: ms_global_policy_vars}
+                      	}
+
+# (2) secrets
+ms_left_secret_vars = {"object_id": context['id'],
+                   	"left": context['left_device_ip'],
+                   	"right": context['right_device_ip'],
+                   	"secret": context['secret'],
+                   	}
+
+sdwan_ipsec_secret_left = {"sdwan_ipsec_secret":
+                       	{context['id']: ms_left_secret_vars}
+                       	}
+
+ms_right_secret_vars = {"object_id": context['id'],
+                    	"left": context['right_device_ip'],
+                    	"right": context['left_device_ip'],
+                    	"secret": context['secret'],
+                    	}
+
+sdwan_ipsec_secret_right = {"sdwan_ipsec_secret":
+                        	{context['id']: ms_right_secret_vars}
+                        	}
+
+# (3) global policy
+
+ms_global_policy_vars = {"object_id": context['id'],
+                     	"ikelifetime": "1440m",
+                     	"keylife": "60m",
+                     	"rekeymargin": "3m",
+                     	"keyingtries": "1",
+                     	"keyexchange": "ikev1",
+                     	"authby": "secret"
+                     	}
+
+sdwan_ipsec_conf_left = {"sdwan_ipsec_conf_policy":
+                     	{context['id']: ms_global_policy_vars}
+                     	}
+
+sdwan_ipsec_conf_right = {"sdwan_ipsec_conf_policy":
+                      	{context['id']: ms_global_policy_vars}
+                      	}
+
+context["sdwan_loopback_left"] = sdwan_loopback_left
+context["sdwan_loopback_right"] = sdwan_loopback_right
+context["sdwan_ipsec_secret_left"] = sdwan_ipsec_secret_left
+context["sdwan_ipsec_secret_right"] = sdwan_ipsec_secret_right
+context["sdwan_ipsec_conf_left"] = sdwan_ipsec_conf_left
+context["sdwan_ipsec_conf_right"] = sdwan_ipsec_conf_right
+context["sdwan_ipsec_conf_crypto_map_left"] = sdwan_ipsec_conf_crypto_map_left
+context["sdwan_ipsec_conf_crypto_map_right"] = sdwan_ipsec_conf_crypto_map_right
+
+
+ret = MSA_API.process_content('ENDED',
+                          	f'IPsec data prepared {context["sdwan_loopback_left"]}',
+                          	context, True)
+
 print(ret)
-
