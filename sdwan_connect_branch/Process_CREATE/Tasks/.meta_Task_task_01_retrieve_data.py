d71990b271f759eecb04ec5fc76339f4ca50434e
jboss <jboss@7f305e094948>
2021-07-13 12:37:32 +0000

    Updating Workflow on  - 13-07-2021 12:37:32 - ncroot

--- sdwan_connect_branch/Process_CREATE/Tasks/.meta_Task_task_01_retrieve_data.py
+++ sdwan_connect_branch/Process_CREATE/Tasks/.meta_Task_task_01_retrieve_data.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1626179773598</value>
+            <value>1626179852126</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1626179773594</value>
+            <value>1626179852121</value>
         </entry>
         <entry>
             <key>TAG</key>
