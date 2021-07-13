55237de30fe25fffc01f1f51463890566a6e48ae
jboss <jboss@7f305e094948>
2021-07-13 12:41:30 +0000

    Updating Workflow on  - 13-07-2021 12:41:30 - ncroot

--- sdwan_connect_branch/Process_CREATE/Tasks/.meta_Task_task_05_create_policy.py
+++ sdwan_connect_branch/Process_CREATE/Tasks/.meta_Task_task_05_create_policy.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1626180080763</value>
+            <value>1626180090439</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1626180080759</value>
+            <value>1626180090435</value>
         </entry>
         <entry>
             <key>TAG</key>
